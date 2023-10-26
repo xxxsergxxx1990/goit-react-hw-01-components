8efb1cb5862089d2f15cbe4e66d930530e8116e0
Sergey Solodovnyk <xxxsergxxx1990@gmail.com>
2023-10-26 19:44:39 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,10 @@
+import {HiBeaker,HiChat} from 'react-icons/hi'
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+   
+      <h1><HiChat/>HELLO!<HiBeaker size = '80'/></h1>
+      
     </div>
-  );
-};
+  )
+}
