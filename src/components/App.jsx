732f7c2aa189274d42dec2d8b6556194b05b4e9d
Sergey Solodovnyk <xxxsergxxx1990@gmail.com>
2023-10-29 16:20:51 +0200

    сделал профиль

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,21 @@
-import {HiBeaker,HiChat} from 'react-icons/hi'
+import user from './data/user.json'
+import data from './data/data.json'
+import friends from './data/friends.json'
+import transactions from './data/transactions.json'
+
+
+import { Profile } from "./Profile/profile";
 export const App = () => {
   return (
-    <div>
    
-      <h1><HiChat/>HELLO!<HiBeaker size = '80'/></h1>
-      
-    </div>
-  )
-}
+      <div>
+         <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      </div>
+  );
+};
